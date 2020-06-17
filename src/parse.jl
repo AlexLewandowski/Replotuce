d258584483650aa0969973c992a400c8924281c6
AlexLewandowski <lewandowskialexp@gmail.com>
2020-06-17 15:11:28 -0600

    try-catch for missing data.jld2

--- src/parse.jl
+++ src/parse.jl
@@ -24,39 +24,43 @@ function gen_dict(;
                 corr_key_val = false
             end
 
-            data = load(string(r, "/", "data.jld2"))
-            settings = load(string(r, "/", "settings.jld2"))
-            parsed = data["parsed"]
+            try:
+                data = load(string(r, "/", "data.jld2"))
+                settings = load(string(r, "/", "settings.jld2"))
+                parsed = data["parsed"]
 
-            primary_metric = data["cb_dict"][primary_metric_key]
+                primary_metric = data["cb_dict"][primary_metric_key]
 
-            metric_keys = collect(keys(data["cb_dict"]))
-            metric_keys_global = copy(metric_keys)
-            secondary_metric_keys = filter!(x -> x != primary_metric_key, metric_keys)
+                metric_keys = collect(keys(data["cb_dict"]))
+                metric_keys_global = copy(metric_keys)
+                secondary_metric_keys = filter!(x -> x != primary_metric_key, metric_keys)
 
-            sweep_param = Dict()
+                sweep_param = Dict()
 
-            for key in sweep_keys
+                for key in sweep_keys
 
-                if key != "seed" && key != "uniqueID"
-                    sweep_param[key] = parsed[key]
-                end
-                if key == sweep_key
-                    if parsed[key] == sweep_val
-                        corr_key_val = true
+                    if key != "seed" && key != "uniqueID"
+                        sweep_param[key] = parsed[key]
+                    end
+                    if key == sweep_key
+                        if parsed[key] == sweep_val
+                            corr_key_val = true
+                        end
                     end
                 end
-            end
 
-            if corr_key_val == true
-                info = Dict([
-                    ("settings", settings),
-                    (primary_metric_key, primary_metric),
-                ])
-                for secondary_metric_key in secondary_metric_keys
-                    info[secondary_metric_key] = data["cb_dict"][secondary_metric_key]
+                if corr_key_val == true
+                    info = Dict([
+                        ("settings", settings),
+                        (primary_metric_key, primary_metric),
+                    ])
+                    for secondary_metric_key in secondary_metric_keys
+                        info[secondary_metric_key] = data["cb_dict"][secondary_metric_key]
+                    end
+                    push_dict!(sweep_dict, sweep_param, info)
                 end
-                push_dict!(sweep_dict, sweep_param, info)
+            catch
+                println(string(r, "/", "data.jld2"), "does not exist!")
             end
 
         end
